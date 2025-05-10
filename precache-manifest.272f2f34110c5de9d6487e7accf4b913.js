1e6cfff87ede5a81d86a4e53b5c5f5d80662615d
seliciayxy <122886918+selixiayxy@users.noreply.github.com>
2025-05-10 13:24:00 +0100

    Updates

---
+++ precache-manifest.272f2f34110c5de9d6487e7accf4b913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366bea0c167cc5723d6707d67f2d691c",
+    "url": "/psyche-studio/index.html"
+  },
+  {
+    "revision": "fbb5d2c5f7393e51b911",
+    "url": "/psyche-studio/static/css/main.b20fc3c4.chunk.css"
+  },
+  {
+    "revision": "4d90ad11f71b9e32d20f",
+    "url": "/psyche-studio/static/js/2.5cf9c239.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/psyche-studio/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "fbb5d2c5f7393e51b911",
+    "url": "/psyche-studio/static/js/main.81a9f2a2.chunk.js"
+  },
+  {
+    "revision": "2a83b2b227c6bad268af",
+    "url": "/psyche-studio/static/js/runtime~main.61bf1463.js"
+  }
+]);
