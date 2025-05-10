6d78de41d237345fcaf668cd7f718cef34872b08
seliciayxy <122886918+selixiayxy@users.noreply.github.com>
2025-05-10 13:26:30 +0100

    updt
    update2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './Pages/HomePage';
 import WorkPage from './Pages/WorkPage';
 import InfoPage from './Pages/InfoPage';
 import Header from './Components/Header';
+import React from 'react';
 import './App.css';
 
 function App() {
